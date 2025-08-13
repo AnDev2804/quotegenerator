713d8a762788f67f5fc2f688d5b8efb869c18b1d
Luis André Mendoza Colmenares <mendozaluisandre030@gmail.com>
2025-08-12 22:37:59 -0400

    Actualizacion de settings.py y urls.py

--- quotegenerator/urls.py
+++ quotegenerator/urls.py
@@ -1,6 +1,9 @@
+# quotegenerator/urls.py
+from django.contrib import admin
 from django.urls import path
-from quotegeneratorapp import views  # Importa tus vistas aquí
+from quotegeneratorapp import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Aquí puedes definir tu vista principal
+    path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
 ]
